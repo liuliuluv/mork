c3dd0816bb7395660d1b47f58df49f574b19d8cd
bones-bones <elliot.ma.brown@gmail.com>
2024-03-22 22:21:15 -0700

    better file name logic

--- acceptCard.py
+++ acceptCard.py
@@ -1,4 +1,5 @@
 import os
+import re
 import discord
 import hc_constants
 from is_mork import getDriveUrl, uploadToDrive
@@ -13,37 +14,42 @@ cardSheetUnapproved = googleClient.open_by_key(hc_constants.HELLSCUBE_DATABASE).
 
 async def acceptCard(bot:commands.Bot, cardMessage:str, file:discord.File, cardName:str, authorName:str):
     cardListChannel = bot.get_channel(hc_constants.FOUR_ONE_CARD_LIST_CHANNEL)
-    await cardListChannel.send(content=cardMessage, file=file)
+    await cardListChannel.send( file, content = cardMessage)
 
-    image_path = f'tempImages/{file.filename}'
+    # this code sucks but i don't remember what the discord file object looks like
+    fileType =(re.search("\.([^.]*)$",file.filename).group())
+
+    print('file',file)
+
+    image_path = f'tempImages/{cardName}{fileType}'
 
     with open(image_path, 'wb') as out: ## Open temporary file as bytes
         out.write(file.fp.read())  ## Read bytes into file
 
-    try:
-        await  postToReddit(
-            title = f"{cardMessage} was accepted!",
-            flair = hc_constants.ACCEPTED_FLAIR,
-            image_path=image_path
-        )
-    except:
-        ...
+        try:
+            await  postToReddit(
+                image_path,
+                title = f"{cardMessage} was accepted!",
+                flair = hc_constants.ACCEPTED_FLAIR
+            )
+        except:
+            ...
 
-    google_drive_file_id = uploadToDrive(image_path)
-    imageUrl = getDriveUrl(google_drive_file_id)
-    os.remove(image_path)
+        google_drive_file_id = uploadToDrive(image_path)
+        imageUrl = getDriveUrl(google_drive_file_id)
+        os.remove(image_path)
 
-    allCardNames = cardSheetUnapproved.col_values(1)
+        allCardNames = cardSheetUnapproved.col_values(1)
 
-    newCard = True
-    if cardName in allCardNames and cardName != "":
-        dbRowIndex = allCardNames.index(cardName) + 1
-        newCard = False
-    else:
-        dbRowIndex = len(allCardNames) + 1
-        if cardName == "":
-            cardName = "NO NAME"
-    cardSheetUnapproved.update_cell(dbRowIndex, 2, imageUrl)
-    if newCard:
-        cardSheetUnapproved.update_cell(dbRowIndex, 1, cardName)
-        cardSheetUnapproved.update_cell(dbRowIndex, 3, authorName)
+        newCard = True
+        if cardName in allCardNames and cardName != "":
+            dbRowIndex = allCardNames.index(cardName) + 1
+            newCard = False
+        else:
+            dbRowIndex = len(allCardNames) + 1
+            if cardName == "":
+                cardName = "NO NAME"
+        cardSheetUnapproved.update_cell(dbRowIndex, 2, imageUrl)
+        if newCard:
+            cardSheetUnapproved.update_cell(dbRowIndex, 1, cardName)
+            cardSheetUnapproved.update_cell(dbRowIndex, 3, authorName)

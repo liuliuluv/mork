20bdbd23c6855776ad3585284eadf6614374befd
bones-bones <elliot.ma.brown@gmail.com>
2024-03-28 19:57:09 -0700

    acceptCard, but it actually fucking waits for the file

--- acceptCard.py
+++ acceptCard.py
@@ -1,5 +1,6 @@
 import os
 import re
+from typing import cast
 import discord
 import hc_constants
 from is_mork import getDriveUrl, uploadToDrive
@@ -13,48 +14,49 @@ cardSheetUnapproved = googleClient.open_by_key(hc_constants.HELLSCUBE_DATABASE).
 
 
 async def acceptCard(bot:commands.Bot, cardMessage:str, file:discord.File, cardName:str, authorName:str):
-
-    cardListChannel = bot.get_channel(hc_constants.FOUR_ONE_CARD_LIST_CHANNEL)
-
+    cardListChannel = cast(discord.TextChannel,bot.get_channel(hc_constants.FOUR_ONE_CARD_LIST_CHANNEL))
     await cardListChannel.send( file = file, content = cardMessage)
 
-# <CIMultiDictProxy('Content-Type': 'image/jpeg', 'Vary': 'Origin', 'Access-Control-Allow-Origin': '*', 'Timing-Allow-Origin': '*', 'Access-Control-Expose-Headers': 'Content-Length', 'Etag': '"v1"', 'Expires': 'Fri, 01 Jan 1990 00:00:00 GMT', 'Cache-Control': 'private, max-age=86400, no-transform', 'Content-Disposition': 'inline;filename="Ancestral Loan.jpg"', 'X-Content-Type-Options': 'nosniff', 'Date': 'Sun, 24 Mar 2024 04:03:48 GMT', 'Server': 'fife', 'Content-Length': '238997', 'X-XSS-Protection': '0')>
+
+    
 
     # this code sucks but i don't remember what the discord file object looks like
-    fileType = re.search("\.([^.]*)$", file.filename).group()
+    extension = re.search("\.([^.]*)$", file.filename)
 
-    image_path = f'tempImages/{cardName.replace("/","|")}{fileType}'
+    fileType = extension.group() if extension else ".png" # just guess that the file is a png
+
+    image_path = f'tempImages/{cardName.replace("/", "|")}{fileType}'
 
     with open(image_path, 'wb') as out: ## Open temporary file as bytes
         out.write(file.fp.read())  ## Read bytes into file
 
-        try:
-            # There used to be a try/catch here, but it turned out that reddit was not the flakiest part here. it was llllll
-            await postToReddit(
-                image_path,
-                title = f"{cardMessage} was accepted!",
-                flair = hc_constants.ACCEPTED_FLAIR
-            )
-        except Exception as e:
-            print(e)
+    try:
+        # There used to be a try/catch here, but it turned out that reddit was not the flakiest part here. it was llllll
+        await postToReddit(
+            image_path,
+            title = f"{cardMessage} was accepted!",
+            flair = hc_constants.ACCEPTED_FLAIR
+        )
+    except Exception as e:
+        print("tried to post to reddit", e)
 
-        google_drive_file_id = uploadToDrive(image_path)
+    google_drive_file_id = uploadToDrive(image_path)
 
-        os.remove(image_path)
+    os.remove(image_path)
 
-        imageUrl = getDriveUrl(google_drive_file_id)
+    imageUrl = getDriveUrl(google_drive_file_id)
 
 
-        allCardNames = cardSheetUnapproved.col_values(1)
-        newCard = True
-        if cardName in allCardNames and cardName != "":
-            dbRowIndex = allCardNames.index(cardName) + 1
-            newCard = False
-        else:
-            dbRowIndex = len(allCardNames) + 1
-            if cardName == "":
-                cardName = "NO NAME"
-        cardSheetUnapproved.update_cell(dbRowIndex, 2, imageUrl)
-        if newCard:
-            cardSheetUnapproved.update_cell(dbRowIndex, 1, cardName)
-            cardSheetUnapproved.update_cell(dbRowIndex, 3, authorName)
+    allCardNames = cardSheetUnapproved.col_values(1)
+    newCard = True
+    if cardName in allCardNames and cardName != "":
+        dbRowIndex = allCardNames.index(cardName) + 1
+        newCard = False
+    else:
+        dbRowIndex = len(allCardNames) + 1
+        if cardName == "":
+            cardName = "NO NAME"
+    cardSheetUnapproved.update_cell(dbRowIndex, 2, imageUrl)
+    if newCard:
+        cardSheetUnapproved.update_cell(dbRowIndex, 1, cardName)
+        cardSheetUnapproved.update_cell(dbRowIndex, 3, authorName)
